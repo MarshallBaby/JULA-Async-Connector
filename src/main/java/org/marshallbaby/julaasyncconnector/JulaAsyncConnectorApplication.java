b3ebe6e24ff8e61d3739d4a3310be0b9b63a2e1a
Alexey Yatsevich <aleksey.yatsevich@gmail.com>
2024-05-18 16:05:39 +0200

    init skeleton

---
+++ src/main/java/org/marshallbaby/julaasyncconnector/JulaAsyncConnectorApplication.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.julaasyncconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JulaAsyncConnectorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JulaAsyncConnectorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/marshallbaby/julaasyncconnector/JulaAsyncConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.marshallbaby.julaasyncconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JulaAsyncConnectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
